1ab023ea99765e0feb70090d7de18b64bfa164d3
BrownBear85 <91921122+BrownBear85@users.noreply.github.com>
2022-11-10 11:50:47 -0800

    auto command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.commands.Autonomous;
 import frc.robot.subsystems.DriveTrain;
 
 /**
@@ -42,7 +43,6 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    // An ExampleCommand will run in autonomous
-    return null;
+    return new Autonomous(m_driveTrain);
   }
 }
