8935fd0c80382a7b4cebc4aff38eebfd38190095
BrownBear85 <91921122+BrownBear85@users.noreply.github.com>
2022-11-09 17:14:30 -0800

    fix

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,7 +8,6 @@ import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.commands.Drive;
 import frc.robot.subsystems.DriveTrain;
 
 /**
